c094e4ec10bb89dc6d3d829f267b2d82c13b845b
Satria <mfsmfsmfs35@gmail.com>
2026-05-14 11:20:01 +0700

    hapus verify admin di get itemRoute.js

--- src/routes/itemRoute.js
+++ src/routes/itemRoute.js
@@ -6,7 +6,7 @@ import { verifyAdmin } from "../../middleware/verifyAdmin.js";
 
 const router = express.Router();
 
-router.get("/api/item", verifyAdmin, getItem);
+router.get("/api/item", getItem);
 router.post("/api/item", verifyAdmin, createItem);
 router.delete("/api/item/:id", verifyAdmin, deleteItem);
 router.put("/api/item/:id", verifyAdmin, editItem);
