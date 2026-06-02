d260e58fea7efbf853356885fc4decb28d902a61
Satria <mfsmfsmfs35@gmail.com>
2026-06-02 15:16:36 +0700

    menambahkan rute get all orders, update order status untuk admin dan memasang verify admin

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { checkout, getOrders } from '../controllers/orderController.js';
+import { checkout, getAllOrders, getOrders, updateOrderStatus } from '../controllers/orderController.js';
+import { verifyAdmin } from '../../middleware/verifyAdmin.js';
 
 const router = express.Router();
 
+// --- Rute untuk Customer ---
 router.get('/', getOrders);        // Untuk melihat riwayat order
 router.post('/checkout', checkout); // Untuk tombol "Checkout"
+
+
+// --- Rute untuk Admin Panel ---
+router.get('/all', verifyAdmin, getAllOrders);
+router.put('/:id/status', verifyAdmin, updateOrderStatus);
 
 export default router;
