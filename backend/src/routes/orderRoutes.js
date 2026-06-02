81d4ce5d1babcbf52fd6550c2d5ea35bcce5354c
Satria <mfsmfsmfs35@gmail.com>
2026-06-02 21:38:20 +0700

    Route baru PUT /:id/complete dengan verifyToken

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { checkout, getAllOrders, getOrders, updateOrderStatus, cancelOrder } from '../controllers/orderController.js';
+import { checkout, getAllOrders, getOrders, updateOrderStatus, cancelOrder, completeOrder } from '../controllers/orderController.js';
 import { verifyAdmin } from '../../middleware/verifyAdmin.js';
 import { verifyToken } from '../../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // --- Rute untuk Customer ---
-router.get('/', verifyToken, getOrders);              // Lihat riwayat order
-router.post('/checkout', verifyToken, checkout);       // Tombol "Checkout"
-router.put('/:id/cancel', verifyToken, cancelOrder);  // Batalkan order (hanya jika 'belum bayar')
+router.get('/', verifyToken, getOrders);               // Lihat riwayat order
+router.post('/checkout', verifyToken, checkout);        // Tombol "Checkout"
+router.put('/:id/cancel', verifyToken, cancelOrder);   // Batalkan order (hanya jika 'belum bayar')
+router.put('/:id/complete', verifyToken, completeOrder); // Konfirmasi selesai (hanya jika 'diproses')
 
 // --- Rute untuk Admin Panel ---
 router.get('/all', verifyAdmin, getAllOrders);
